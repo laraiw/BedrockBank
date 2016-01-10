67432e83dd03c4f2e2b34e3f279c6c26dc25d737
Larai W <laraiw@hotmail.com>
2016-01-09 21:10:49 -0800

    Added SSN

--- BedrockBank/Program.cs
+++ BedrockBank/Program.cs
@@ -10,45 +10,49 @@ namespace BedrockBank
     {
         static void Main(string[] args)
         {
-            var account = BankFactory.CreateAccount("Larai's Checking", 100); /* replaces next 4 lines */
-            //Create an instance of an account == object
-            // var account = new Account();
-            // account.Name = "Larai's Checking";
-            // account.Deposit(400);
+            Console.WriteLine("Welcome to Bedrock Bank!");
+            Console.Write("Please login with your SSN: ");
+            var ssn = Console.ReadLine();
 
-            Console.WriteLine(
-                "Name: {0}, AccountNumber: {1}; Balance: {2:c}", 
-                account.Name, account.AccountNumber, 
-                account.Balance
-                );
+            // var account = BankFactory.CreateAccount("Larai's Checking", 100); /* replaces next 4 lines */
+            // //Create an instance of an account == object
+            // // var account = new Account();
+            // // account.Name = "Larai's Checking";
+            // // account.Deposit(400);
 
-            var savingsaccount = BankFactory.CreateAccount("Larai's Savings", 100);
-            //var savingsaccount = new Account();  //same as - Account savingsaccount = new Account(); "var" figures it out
-            //savingsaccount.Name = "Larai's Savings";
-            //savingsaccount.Deposit(10);
-            //savingsaccount.Withdraw(100);
+            // Console.WriteLine(
+            //     "Name: {0}, AccountNumber: {1}; Balance: {2:c}", 
+            //     account.Name, account.AccountNumber, 
+            //     account.Balance
+            //     );
 
-            //Exception Handling
+            // var savingsaccount = BankFactory.CreateAccount("Larai's Savings", 100);
+            // //var savingsaccount = new Account();  //same as - Account savingsaccount = new Account(); "var" figures it out
+            // //savingsaccount.Name = "Larai's Savings";
+            // //savingsaccount.Deposit(10);
+            // //savingsaccount.Withdraw(100);
 
-           try
-            {
-                savingsaccount.Withdraw(100);
-            }
-            catch (ArgumentException ex)
-            {
-                Console.WriteLine(
-                    "Oops, somethings went wrong {0}",
-                     ex.Message);
-            }
-            catch(Exception)
-            {
-                Console.WriteLine("Yikes, somethings went wrong!");
-            }
-            Console.WriteLine(
-                "Name: {0}, AccountNumber: {1}; Balance: {2:c}",
-                savingsaccount.Name, savingsaccount.AccountNumber,
-                savingsaccount.Balance
-                );
+            // //Exception Handling
+
+            //try
+            // {
+            //     savingsaccount.Withdraw(100);
+            // }
+            // catch (ArgumentException ex)
+            // {
+            //     Console.WriteLine(
+            //         "Oops, somethings went wrong {0}",
+            //          ex.Message);
+            // }
+            // catch(Exception)
+            // {
+            //     Console.WriteLine("Yikes, somethings went wrong!");
+            // }
+            // Console.WriteLine(
+            //     "Name: {0}, AccountNumber: {1}; Balance: {2:c}",
+            //     savingsaccount.Name, savingsaccount.AccountNumber,
+            //     savingsaccount.Balance
+            //     );
 
         }
     }
